3519142a3f1c5469e176ce1608a81543639b548f
Steve Kane <kanesteven@gmail.com>
2021-09-12 01:43:39 -0500

    refactored some utility functions to make them more meaningful. fixed or completed more opcodes

--- app/Main.hs
+++ app/Main.hs
@@ -3,9 +3,12 @@ module Main where
 import Data.Bits (shiftL, shiftR, (.&.), (.|.), xor)
 import Data.Array (Array, array, (!), (//))
 import Data.Word (Word8, Word16, Word32)
+import Data.Vector.Unboxed (Vector)
 import System.Random 
 import Lib
+import Data.Semigroup (diff)
 
+-- Type aliases
 type RAMAddress = Word16
 type StackAddress = Word8
 type RegisterAddress = Word8
@@ -16,153 +19,209 @@ type Registers = Array RegisterAddress Word8
 type Nibbles = (Word8, Word8, Word8, Word8)
 type OpCode = (Word8, Word8, Word8, Word8, Word8, Word16)
 
+-- Model for Chip8 CPU
 data Chip8 = Chip8 {
   randomSeed :: StdGen,
-  inputs     :: Array Word8 InputState,        -- 16 booleans
   display    :: Array DisplayAddress Pixel,    -- 64 × 32 booleans indexed by a 32-bit Word
+  inputs     :: Array Word8 InputState,        -- 16 booleans
   d          :: Word8,                         -- byte delay timer
   s          :: Word8,                         -- byte sound timer
   pc         :: RAMAddress,                    -- 2-byte program counter
-  sp         :: StackAddress,                  -- stack pointer
+  sp         :: StackAddress,                  -- 1-byte stack pointer
   i          :: RAMAddress,                    -- 2-byte index register
   registers  :: Array RegisterAddress Word8,   -- 16 bytes
-  stack      :: Array StackAddress RAMAddress, -- stack of 2-byte adresses
+  stack      :: Array StackAddress RAMAddress, -- 16 2-byte adresses
   ram        :: Array RAMAddress Word8         -- 4096 bytes indexed by word16
 } deriving (Show)
 
+-- Constants
+instructionByteWidth = 2
+displayWidth = 64
+displayHeight = 32
 off = False
 on = True
-step pc = pc + 2
-skipIf b = if b then 4 else 2
-carry a b = if (word16 a + word16 b) > 255 then 1 else 0
-borrow a b = if a > b then 0 else 1
-lsb b = if nthbit 0 b then 1 else 0
-msb b = if nthbit 7 b then 1 else 0
-initialize (min,max) v0 = array (min,max) [(i,v0) | i <- [min..max]]
+blankDisplay = initialize (0,displayWidth * displayHeight - 1) off
 
+-- Semantic helper functions
+step pc = pc + instructionByteWidth
+
+skipIf pc True = pc + instructionByteWidth + instructionByteWidth
+skipIf pc False = pc + instructionByteWidth
+
+blockIf pc True = pc
+blockIf pc False = pc + instructionByteWidth
+
+initialize (min,max) v0 = array (min,max) (fmap initialPair [min..max])
+  where initialPair i = (i,v0)
+
+copyTo (to,t0) (from,f0) n = to // fmap updatePair [0..n]
+  where updatePair i = (t0 + fromIntegral i,from ! (f0 + fromIntegral i))
+
+-- OpCode transformations
 callSubroutineAtNNN nnn cpu = cpu { 
   pc = nnn, 
   sp = sp cpu + 1,
   stack = stack cpu // [(sp cpu,pc cpu)]
 }
+
 returnFromSubroutine cpu = cpu { 
   pc = stack cpu ! sp cpu,
   sp = sp cpu - 1
 }
+
 jumpToNNN nnn cpu = cpu { 
   pc = nnn 
 }
+
 jumpToV0PlusNNN v0 nnn cpu = cpu { 
   pc = word16 v0 + nnn 
 }
+
 skipIfVxIsNN vx nn cpu = cpu { 
-  pc = pc cpu + skipIf (vx == nn) 
+  pc = skipIf (pc cpu) (vx == nn) 
 }
+
 skipUnlessVxIsNN vx nn cpu = cpu { 
-  pc = pc cpu + skipIf (vx /= nn) 
+  pc = skipIf (pc cpu) (vx /= nn) 
 }
+
 skipIfVxIsVy vx vy cpu = cpu { 
-  pc = pc cpu + skipIf (vx == vy) 
+  pc = skipIf (pc cpu) (vx == vy) 
 }
+
 skipUnlessVxIsVy vx vy cpu = cpu { 
-  pc = pc cpu + skipIf (vx /= vy) 
+  pc = skipIf (pc cpu) (vx /= vy) 
 }
+
 setVxToNN x vx nn cpu = cpu { 
   pc = step (pc cpu), 
   registers = registers cpu // [(x,nn)] 
 }
+
 setVxToVxPlusNN x vx nn cpu = cpu { 
   pc = step (pc cpu), 
   registers = registers cpu // [(x,vx + nn)] 
 }
+
 setVxToVy x vx vy cpu = cpu { 
   pc = step (pc cpu), 
   registers = registers cpu // [(x,vy)] 
 }
+
 setVxToVxOrVy x vx vy cpu = cpu { 
   pc = step (pc cpu), 
   registers = registers cpu // [(x,vx .|. vy)] 
 }
+
 setVxToVxAndVy x vx vy cpu = cpu { 
   pc = step (pc cpu), 
   registers = registers cpu // [(x,vx .&. vy)] 
 }
+
 setVxToVxXorVy x vx vy cpu = cpu { 
   pc = step (pc cpu), 
   registers = registers cpu // [(x,vx `xor` vy)] 
 }
+
 setVxToVxPlusVy x vx vy cpu = cpu {
   pc = step (pc cpu),
-  registers = registers cpu // [(x,vx + vy), (0xF,carry vx vy)]
-}
+  registers = registers cpu // [(x,sum), (0xF,toWord8 carry)]
+} where (sum, carry) = vx `addWithCarry` vy
+
 setVxToVxMinusVy x vx vy cpu = cpu {
   pc = step (pc cpu),
-  registers = registers cpu // [(x,vx - vy), (0xF,borrow vx vy)]
-}
+  registers = registers cpu // [(x,difference), (0xF,toWord8 borrow)]
+} where (difference, borrow) = vx `subtractWithBorrow` vy
+
 setVxToVyMinusVx x vx vy cpu = cpu {
   pc = step (pc cpu),
-  registers = registers cpu // [(x,vy - vx), (0xF,borrow vy vx)]
-}
-leftShiftVxAndStoreLSBVx x vx cpu = cpu {
+  registers = registers cpu // [(x,difference), (0xF,toWord8 borrow)]
+} where (difference, borrow) = vy `subtractWithBorrow` vx
+
+rightShiftVxAndStoreLSBVx x vx cpu = cpu {
   pc = step (pc cpu),
-  registers = registers cpu // [(x,vx `shiftR` 1), (0xF,lsb vx)]
-}
-rightShiftVxAndStoreMSBVx x vx cpu = cpu {
+  registers = registers cpu // [(x,vx `shiftR` 1), (0xF,lsbvx)]
+} where lsbvx = toWord8 (nthbit 0 vx)
+
+leftShiftVxAndStoreMSBVx x vx cpu = cpu {
   pc = step (pc cpu),
-  registers = registers cpu // [(x,vx `shiftL` 1), (0xF,msb vx)]
-}
+  registers = registers cpu // [(x,vx `shiftL` 1), (0xF,msbvx)]
+} where msbvx = toWord8 (nthbit 7 vx)
+
 setVxToRandAndNN x nn cpu = cpu {
   pc = step (pc cpu),
   registers = registers cpu // [(x,randValue .&. nn)],
   randomSeed = randomSeed'
 } where (randValue, randomSeed') = genWord8 (randomSeed cpu)
+
+clearDisplay cpu = cpu {
+  pc = step (pc cpu),
+  display = blankDisplay
+}
+
 setIToNNN nnn cpu = cpu {
   pc = step (pc cpu),
   i = nnn
 }
+
 setIToIPlusVx vx cpu = cpu {
   pc = step (pc cpu),
   i = i cpu + word16 vx
 }
+
 setIToISpriteAddressVx vx cpu = cpu {
   pc = step (pc cpu),
   i = word16 vx * fontHeight
 } where fontHeight = 5
+
+storeBCDVxAtI vx cpu = cpu {
+  pc = step (pc cpu),
+  ram = ram cpu // [(i cpu,hundreds), (i cpu + 1,tens), (i cpu + 2,ones)]
+} where (hundreds, tens, ones) = digits vx
+
 dumpRegistersV0ToVxToI x cpu = cpu {
   pc = step (pc cpu),
-  ram = ram cpu // [(i cpu + word16 offset,registers cpu ! offset) | offset <- [0..x]]
+  ram = copyTo (ram cpu,i cpu) (registers cpu,0) x
 }
+
 loadRegistersV0ToVxFromI x cpu = cpu {
   pc = step (pc cpu),
-  registers = registers cpu // [(offset,ram cpu ! (i cpu + word16 offset)) | offset <- [0..x]]
+  registers = copyTo (registers cpu,0) (ram cpu,i cpu) x
 }
+
 setVxToD x cpu = cpu {
   pc = step (pc cpu),
   registers = registers cpu // [(x,d cpu)]
 }
+
 setDToVx vx cpu = cpu {
   pc = step (pc cpu),
   d = vx
 }
+
 setSToVx vx cpu = cpu {
   pc = step (pc cpu),
   s = vx
 }
+
 skipIfKeyDownVx vx cpu = cpu {
-  pc = pc cpu + skipIf (inputs cpu ! vx) 
-}
-skipUnlessKeyDownVx vx cpu = cpu {
-  pc = pc cpu + skipIf (not (inputs cpu ! vx))
-}
-blockUnlessKeyDownVx vx cpu = cpu {
-  pc = if inputs cpu ! vx then pc cpu else step (pc cpu)
+  pc = skipIf (pc cpu) (inputs cpu ! vx) 
 }
 
+skipUnlessKeyDownVx vx cpu = cpu {
+  pc = skipIf (pc cpu) (not (inputs cpu ! vx))
+}
+
+blockUnlessKeyDownVx vx cpu = cpu {
+  pc = blockIf (pc cpu) (not (inputs cpu ! vx))
+}
+
+-- Construct new CPU with random seed and program
 load :: StdGen -> () -> Chip8
 load randomSeed program = Chip8 {
   randomSeed = randomSeed,
+  display = blankDisplay,
   inputs = initialize (0,15) off,
-  display = initialize (0,64 * 32 - 1) off,
   registers = initialize (0,15) 0, 
   stack = initialize (0,15) 0,
   ram = initialize (0,4095) 0,
@@ -173,12 +232,14 @@ load randomSeed program = Chip8 {
   i = 0
 }
 
+-- fetch two bytes from cpu at current pc and split into 4 nibbles
 fetch :: Chip8 -> Nibbles
 fetch cpu = 
   let b0 = ram cpu ! pc cpu
       b1 = ram cpu ! (pc cpu + 1)
   in  (highNibble b0, lowNibble b0, highNibble b1, lowNibble b1)
 
+-- extract commonly-used values from cpu registers and instruction nibbles
 decode :: Nibbles -> Chip8 -> OpCode
 decode (_,x,y,z) cpu =
   let vx = registers cpu ! x
@@ -189,7 +250,7 @@ decode (_,x,y,z) cpu =
       n = z
   in  (vx, vy, v0, n, nn, nnn)
   
-
+-- fetch, decode and execute the next opcode
 execute :: Chip8 -> Chip8
 execute cpu = 
   let nibbles = fetch cpu
@@ -212,15 +273,15 @@ execute cpu =
     (0x8, x, y, 0x4)     -> setVxToVxPlusVy x vx vy cpu
     (0x8, x, y, 0x5)     -> setVxToVxMinusVy x vx vy cpu
     (0x8, x, y, 0x7)     -> setVxToVyMinusVx x vx vy cpu
-    (0x8, x, y, 0x6)     -> leftShiftVxAndStoreLSBVx x vx cpu
-    (0x8, x, y, 0xE)     -> rightShiftVxAndStoreMSBVx x vx cpu
+    (0x8, x, y, 0x6)     -> rightShiftVxAndStoreLSBVx x vx cpu
+    (0x8, x, y, 0xE)     -> leftShiftVxAndStoreMSBVx x vx cpu
     (0xC, x, _, _)       -> setVxToRandAndNN x nn cpu
-    -- (0x0,0x0,0xE,0x0)    -> clear cpu
+    (0x0,0x0,0xE,0x0)    -> clearDisplay cpu
     -- (0xD, _, _, _)       -> draw vx vy n cpu
     (0xA, _, _, _)       -> setIToNNN nnn cpu
     (0xF, _, 0x1, 0xE)   -> setIToIPlusVx vx cpu
     (0xF, _, 0x2, 0x9)   -> setIToISpriteAddressVx vx cpu
-    -- (0xF, _, 0x3, 0x3)   -> storeBCDVxAtI vx cpu
+    (0xF, _, 0x3, 0x3)   -> storeBCDVxAtI vx cpu
     (0xF, x, 0x5, 0x5)   -> dumpRegistersV0ToVxToI x cpu
     (0xF, x, 0x6, 0x5)   -> loadRegistersV0ToVxFromI x cpu
     (0xF, x, 0x0, 0x7)   -> setVxToD x cpu
@@ -236,4 +297,5 @@ chip8 = load rndSeed ()
 
 main :: IO ()
 main = do
+  print $ digits 0xf3
   print $ fetch chip8
--- src/Lib.hs
+++ src/Lib.hs
@@ -3,7 +3,21 @@ module Lib where
 import Data.Word (Word8, Word16)
 import Data.Bits 
 
+toWord8 :: Bool -> Word8
+toWord8 True = 0x1
+toWord8 False = 0x0
+
+digits :: Word8 -> (Word8, Word8, Word8)
+digits b = (hundreds, tens, ones)
+  where
+    ones = b `mod` 10
+    tens = b `div` 10 `mod` 10
+    hundreds = b `div` 100 `mod` 10
+
+highNibble :: Word8 -> Word8 -- no available Word4 type
 highNibble b = (b .&. 0xF0) `shiftR` 4
+
+lowNibble :: Word8 -> Word8 -- no available Word4 type
 lowNibble b = b .&. 0x0F
 
 nthbit :: Int -> Word8 -> Bool
@@ -19,4 +33,10 @@ word8FromNibbles :: Word8 -> Word8 -> Word8
 word8FromNibbles h l = (h `shiftL` 4) + l
 
 word16FromNibbles :: Word8 -> Word8 -> Word8 -> Word16
-word16FromNibbles h m l = fromIntegral h `shiftL` 8 + fromIntegral m `shiftL` 4 + fromIntegral l
+word16FromNibbles h m l = (word16 h `shiftL` 8) + (word16 m `shiftL` 4) + word16 l
+
+addWithCarry :: Word8 -> Word8 -> (Word8, Bool)
+addWithCarry a b = (a + b, (word16 a + word16 b) > 255)
+
+subtractWithBorrow :: Word8 -> Word8 -> (Word8, Bool)
+subtractWithBorrow a b = (a - b, b < a)
